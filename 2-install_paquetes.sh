074c55024f10652973ea20aa99ada6d9f577d74f
Fernando Korol <fkorol@siu.edu.ar>
2026-08-03 10:42:55 -0300

    setup inicial ubuntu

--- 2-install_paquetes.sh
+++ 2-install_paquetes.sh
@@ -1,5 +1,14 @@
-# 8. Navegadores, VS Code y LM Studio
+# Add Docker's official GPG key:
+# Definir modo no interactivo para apt
+export DEBIAN_FRONTEND=noninteractive
+
+#gnome:
+sudo apt-get install -y git ssh  postgresql-client meld zsh vlc git-flow p7zip-full gnome-shell-extensions chrome-gnome-shell geany mc gimp curl unzip htop deluge powertop nmap make gcc fonts-powerline gnome-tweaks ubuntu-restricted-extras rar unrar powerstat build-essential cpu-x gnome-system-monitor stress libfuse2 fzf fontconfig flatpak direnv pkg-config
+
+
+# 1. Navegadores, VS Code y LM Studio
 echo "--> Instalando Google Chrome, Opera, Visual Studio Code y LM Studio..."
+
 
 # -----------------------------------------------------------------------------
 # Google Chrome
@@ -49,6 +58,28 @@ sudo apt install -y \
 
 
 
+
+#Pentaho / Spoon
+sudo apt install -y openjdk-11-jdk
+
+
+echo "paquetes flatpak"
+
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+flatpak install -y flathub \
+com.getpostman.Postman \
+org.pgadmin.pgadmin4 \
+info.febvre.Komikku \
+com.vivaldi.Vivaldi \
+net.waterfox.waterfox \
+com.rtosta.zapzap \
+org.telegram.desktop \
+com.obsproject.Studio \
+org.zotero.Zotero
+
+
+
 # -----------------------------------------------------------------------------
 # LM Studio (última versión .deb)
 # -----------------------------------------------------------------------------
@@ -65,3 +96,44 @@ if ! dpkg -s lmstudio >/dev/null 2>&1; then
 
     rm -f "$TMP_DEB"
 fi
+
+
+git config --global credential.helper store
+git config --global core.editor "nano"
+git config --global user.email "ferkorol@gmail.com"
+git config --global user.name "Fernando Korol"
+
+
+
+
+echo "Instalacion DOCKER"
+
+sudo apt update -y
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Agregar el repositorio a las fuentes de Apt:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Architectures: $(dpkg --print-architecture)
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update -y
+
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+
+echo "Permisos a .kube y .ssh"
+
+
+chmod 700 ~/.ssh
+chmod 600 ~/.ssh/*
+
+chmod 700 ~/.kube
+chmod 600 ~/.kube/*
